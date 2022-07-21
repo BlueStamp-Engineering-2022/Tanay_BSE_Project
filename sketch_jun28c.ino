e3b164ee77084f6dfc4a2b96335e07c9fc878fa3
tubale <43082103+tubale@users.noreply.github.com>
2022-07-21 00:15:31 -0700

    Add files via upload

--- sketch_jun28c.ino
+++ sketch_jun28c.ino
@@ -1,13 +1,26 @@
-#include "LedControlMS.h"
+ #include "LedControlMS.h"
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+#include <Servo.h>
 
 
 const int trigPin = 7;
 const int echoPin = 8;
+const int ledPin = 11;
+char inputByte;
+int randomNumber;
+int pos;
+int number;
 
-int randomNumber = 1;
+//Emotions
+/*
+ * Love --> random motion
+ * Sad --> slow motion
+ * Angry --> Jerky
+ *
+*/
+
 LiquidCrystal_I2C lcd(0x27, 16, 1);
 //define variables
 
@@ -18,8 +31,12 @@ LiquidCrystal_I2C lcd(0x27, 16, 1);
 #define HAPPY 3
 #define ANGRY 4
 #define LOVE 5
+#define SAD 6
 
-
+Servo MG995_tilt;
+Servo MG996_pan;
+Servo LEFT_BROW;
+Servo RIGHT_BROW;
 
 
 int emotion = NEUTRAL;
@@ -35,9 +52,12 @@ byte happy[8] = {0x00, 0x1c, 0x24, 0x5c, 0x5c, 0x24, 0x1c, 0x00};
 byte angry[8] = {0x00, 0x3c, 0x42, 0x5a, 0x5a, 0x42, 0x3c, 0x00};
 byte surprised[8] = {0x7e, 0x81, 0x81, 0x99, 0x99, 0x81, 0x81, 0x7e};
 byte love[8] = {0x00, 0x66, 0x99, 0x81, 0x42, 0x24, 0x18, 0x00};
+byte sad[8] = {0x00, 0x0c, 0x12, 0x3a, 0x5a, 0x42, 0x3c, 0x00};
 
 void setup()
 {
+
+  int randomNumber = 1;
 
   lc.shutdown(0, false);
   lc.shutdown(1, false);
@@ -48,7 +68,6 @@ void setup()
   lc.clearDisplay(0);
   lc.clearDisplay(1);
 
-
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an OUTPUT
   pinMode(echoPin, INPUT); // Sets the echoPin as an INPUT
   Serial.begin(9600); // // Serial Communication is starting with 9600 of baudrate speed
@@ -58,26 +77,125 @@ void setup()
   lcd.init();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  if (randomNumber == 1)
-  {
-    lcd.println("I am Happy       ");
-  }
-  else
-  {
-    lcd.println("I am Angry        ");
-  }
+  
   //lcd.println("Distance: ");
+
+
+  // We need to attach the servo to the used pin number
+  MG995_tilt.attach(6);
+  MG996_pan.attach(9);
+  LEFT_BROW.attach(A3);
+  RIGHT_BROW.attach(A2);
+  
 
 
 
 }
 
+void neutralMethod() {
+  lc.clearDisplay(0);
+  lc.clearDisplay(1);
+  emotion = NEUTRAL;
+  printByte(neutral);
+  LEFT_BROW.write(90);
+  RIGHT_BROW.write(90);
+
+  delay(100);
+MG996_pan.write(20);
+  MG995_tilt.write(10);  
+
+  delay(100);
+}
+
+void surprisedMethod() {
+  lc.clearDisplay(0);
+  lc.clearDisplay(1);
+  emotion = SURPRISED;
+  printByte(surprised);
+
+  LEFT_BROW.write(135);
+  RIGHT_BROW.write(45);
+  delay(100);
+  LEFT_BROW.write(30);
+  RIGHT_BROW.write(120);
+  delay(100);
+  LEFT_BROW.write(135);
+  RIGHT_BROW.write(45);
+  delay(100);
+  MG996_pan.write(20);
+  MG995_tilt.write(10);  
+ 
+
+  delay(100);
+}
+void loveMethod() {
+  lc.clearDisplay(0);
+  lc.clearDisplay(1);
+  emotion = LOVE;
+  printByte(love);
+
+  LEFT_BROW.write(135);
+  RIGHT_BROW.write(45);
+  delay(100);
+  LEFT_BROW.write(90);
+  RIGHT_BROW.write(0);
+  delay(100);
+  LEFT_BROW.write(135);
+  RIGHT_BROW.write(45);
+
+  delay(10);
+  MG996_pan.write(20);
+  MG995_tilt.write(10);  
+  delay(100);
+}
+void happyMethod() {
+  lc.clearDisplay(0);
+  lc.clearDisplay(1);
+  emotion = HAPPY;
+  printByte(happy);
+
+    LEFT_BROW.write(90);
+  RIGHT_BROW.write(90);
+  delay(100);
+
+  LEFT_BROW.write(135);
+  RIGHT_BROW.write(45);
+  delay(100);
+  MG996_pan.write(20);
+  MG995_tilt.write(10);  
+  delay(100);
+  
+}
+
+void angryMethod() {
+  lc.clearDisplay(0);
+  lc.clearDisplay(1);
+  emotion = ANGRY;
+  printByte(angry);
 
 
+  LEFT_BROW.write(45);
+  RIGHT_BROW.write(135);
+  delay(100);
+}
+void sadMethod() {
+  lc.clearDisplay(0);
+  lc.clearDisplay(1);
+  emotion = SAD;
+  printByte(sad);
+
+
+  LEFT_BROW.write(120);
+  RIGHT_BROW.write(60);
+  delay(100);
+
+
+  
+ }
 
 void loop()
 {
-
+  Serial.println("test");
   // Clears the trigPin condition
   digitalWrite(trigPin, LOW);
   delay(2);
@@ -91,8 +209,26 @@ void loop()
   // Calculating the distance
   distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
   // Displays the distance on the Serial Monitor
+  delay(10);
   Serial.print("distance ");
   Serial.println(distance);
+
+   while(Serial.available()>0)
+  {
+    inputByte= Serial.read();
+    Serial.println(inputByte);
+    if (inputByte=='Z')
+    {
+    randomNumber = 1;
+    }
+    else 
+    {
+      randomNumber = 2;
+    }
+    //else if (inputByte=='z'){
+    //digitalWrite(13,LOW); 
+  }
+  Serial.println(randomNumber);
   /*lcd.setCursor(0, 1);
     if (distance < 200) {
       lcd.print(distance);
@@ -102,33 +238,30 @@ void loop()
     lcd.print("Error       ");
     }*/
 
+    if (randomNumber == 1)
+  {
+    lcd.println("I am Happy            ");
+  }
+  else
+  {
+    lcd.println("I am Angry            ");
+  }
+
   if (randomNumber == 1) {
     if (distance <= 5 && emotion != SURPRISED)  {
-      lc.clearDisplay(0);
-      lc.clearDisplay(1);
-      emotion = SURPRISED;
-      printByte(surprised);
+      surprisedMethod();
     }
-    else if (5 < distance && distance <= 15 && emotion != LOVE )
+    else if (5 < distance && distance <= 10 && emotion != LOVE )
     {
-      lc.clearDisplay(0);
-      lc.clearDisplay(1);
-      emotion = LOVE;
-      printByte(love);
+      loveMethod();
     }
-    else if (15 < distance && distance < 25 && emotion != HAPPY)
+    else if (10 < distance && distance < 25 && emotion != HAPPY)
     {
-      lc.clearDisplay(0);
-      lc.clearDisplay(1);
-      emotion = HAPPY;
-      printByte(happy);
+      happyMethod();
     }
     else if (distance >= 25 && emotion != NEUTRAL)
     {
-      lc.clearDisplay(0);
-      lc.clearDisplay(1);
-      emotion = NEUTRAL;
-      printByte(neutral);
+      neutralMethod();
     }
   }
 
@@ -136,33 +269,33 @@ void loop()
 
   else {
 
-    if (distance <= 5) {
-      lc.clearDisplay(0);
-      printByte(surprised);
+    if (distance <= 5 && emotion != SURPRISED)  {
+      surprisedMethod();
     }
-    else if (5 < distance && distance <= 15)
+    else if (5 < distance && distance <= 10 && emotion != NEUTRAL )
     {
-      lc.clearDisplay(0);
-      printByte(love);
+      neutralMethod();
     }
-    else if (15 < distance && distance < 25)
+    else if (10 < distance && distance < 25 && emotion != ANGRY)
     {
-      lc.clearDisplay(0);
-      printByte(angry);
+      angryMethod();
     }
 
-    else
+    else if (distance >= 25 && emotion != SAD)
     {
-      lc.clearDisplay(0);
-      printByte(neutral);
+      
+      sadMethod();
     }
+
   }
 
 
 
 
 
-  delay(100);
+  delay(1000);
+
+  lcd.clear();
 }
 
 void printByte(byte character [])
